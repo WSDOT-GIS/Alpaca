5013dbc3d743204142c778c273bdd22d1b24a3d9
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-07-17 12:48:34 -0700

    Moved drop down button definition to markup.

--- Scripts/index.js
+++ Scripts/index.js
@@ -8,7 +8,7 @@
 	"dojo/parser",
 	"dijit/layout/ContentPane",
 	"dijit/layout/BorderContainer"
-], function (ready, registry, Map, BasemapGallery, DropDownButton, TooltipDialog) {
+], function (ready, registry, Map, BasemapGallery) {
 	ready(function () {
 		var map;
 
@@ -20,23 +20,6 @@
 		});
 
 		map.on("load", function () {
-			var basemapButton, basemapDialog;
-			
-			basemapButton = document.createElement("div");
-			basemapButton.id = "basemapButton";
-
-			map.root.appendChild(basemapButton);
-
-			basemapDialog = new TooltipDialog({
-				content: '<div id="basemapGallery"></div>'
-			});
-
-			basemapButton = new DropDownButton({
-				label: "Basemap",
-				name: "basemap",
-				dropDown: basemapDialog
-			}, basemapButton);
-
 		});
 	});
 });
