d0c7d2978f12501015de58f39135f41ab6cdc77d
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-07-18 15:59:31 -0700

    Added Extensions.cs.

---
+++ Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+
+namespace TitleVI
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Determines if the <see cref="HttpRequest.Url"/> is from the same site as the <see cref="HttpRequest.UrlReferrer"/>
+		/// in a given <see cref="HttpContext"/>.
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns>
+		/// Returns <see langword="null"/> if there is no <see cref="HttpRequest.UrlReferrer"/>, 
+		/// <see langword="true"/> if there is a match, <see langword="false"/> if not.
+		/// </returns>
+		public static bool? IsReferrerSameSite(this HttpRequest request)
+		{
+			// If there is no referrer, return false.
+			if (request.UrlReferrer == null)
+			{
+				return null;
+			}
+
+			// Get the root part of the URL...
+			Regex re = new Regex(@"/^https?\:\/\/[^\/]+/", RegexOptions.IgnoreCase);
+			// Create a Regex that will match if the referrer URL matches the current URL.
+			Match m = re.Match(request.Url.ToString());
+			re = new Regex("^" + Regex.Escape(m.Value), RegexOptions.IgnoreCase);
+
+			// Return true if the referrer is a match, false otherwise.
+			return re.IsMatch(request.UrlReferrer.ToString());
+		}
+	}
+}
