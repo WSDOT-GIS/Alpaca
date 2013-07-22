b54852bef247decee4e7f476b8f3f7d5a5c4f888
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-07-22 10:59:14 -0700

    Added commented-out code for loading from a web map.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,9 @@
 ﻿/*global require*/
 require([
 	"dojo/ready",
+	"esri/config",
+	"esri/urlUtils",
+	"esri/arcgis/utils",
 	"esri/map",
 	"esri/dijit/BasemapGallery",
 	"esri/dijit/Legend",
@@ -12,11 +15,51 @@ require([
 	"dijit/layout/BorderContainer",
 	"dijit/layout/TabContainer",
 	"dijit/form/Button"
-], function (ready, Map, BasemapGallery, Legend) {
+], function (ready, config, urlUtils, arcgisUtils, Map, BasemapGallery, Legend) {
 	"use strict";
 
+	////var mapId = "f18bec1c4af74955a02d8647e1495c20";
+
+	////config.defaults.io.proxyUrl = "proxy.ashx";
+
+	////urlUtils.addProxyRule({
+	////	proxyUrl: "proxy.ashx",
+	////	urlPrefix: location.protocol + "//www.arcgis.com/sharing/rest/content/items/" + mapId
+	////});
+
+	////urlUtils.addProxyRule({
+	////	proxyUrl: "proxy.ashx",
+	////	urlPrefix: location.protocol + "//demographics1.arcgis.com"
+	////});
+
 	ready(function () {
-		var map, mapId = "f18bec1c4af74955a02d8647e1495c20";
+		var map;
+
+		////arcgisUtils.createMap(mapId, "map", {
+		////	mapOptions: {
+		////		basemap: "gray",
+		////		center: [-120.80566406246835, 47.41322033015946],
+		////		zoom: 7,
+		////		showAttribution: true
+		////	}
+		////}).then(function (response) {
+		////	map = response.map;
+
+		////	map.on("load", function () {
+		////		var basemapGallery, legend;
+
+		////		basemapGallery = new BasemapGallery({
+		////			map: map
+		////		}, "basemapGallery");
+
+		////		basemapGallery.startup();
+
+		////		legend = new Legend({
+		////			map: map,
+		////			autoUpdate: true
+		////		}, "legend");
+		////	});
+		////});
 
 		map = new Map("map", {
 			basemap: "gray",
