d66c50e28f4c225d2daed3d05a098036c4a16348
JeffJacobson <Jeffrey.M.Jacobson@gmail.com>
2013-08-21 17:09:52 -0700

    Added JSDoc comments to raceData.js.

--- Scripts/wsdot/title6/raceData.js
+++ Scripts/wsdot/title6/raceData.js
@@ -3,10 +3,17 @@
 define(function () {
 	"use strict";
 
+	/**
+	 * @constructor
+	 */
 	function RaceData(/**{Object}*/ queryResults) {
+		/** @member {!number} */
 		this.white = queryResults.SUM_White || queryResults.white || 0;
+		/** @member {!number} */
 		this.minority = queryResults.SUM_NotWhite || queryResults.minority || 0;
+		/** @member {!number} */
 		this.oneRace = queryResults.SUM_OneRace || queryResults.oneRace || 0;
+		/** @member {Object.<string, number>} */
 		this.marginOfError = queryResults.marginOfError || {
 			white: queryResults.MAX_MEWhite,
 			oneRace: queryResults.MAX_MEOneRace,
@@ -14,15 +21,24 @@ define(function () {
 		};
 	}
 
+	/** @static {Object.<string, string>} */
 	RaceData.labels = {
+		/** @member {string} */
 		white: "White",
+		/** @member {string} */
 		minority: "Minority"
 	};
 
+	/** Returns the total number of people.
+	 * @returns {number}
+	 */
 	RaceData.prototype.getTotal = function () {
 		return this.white + this.minority;
 	};
 
+	/** Creates objects used to populate a pie chart.
+	 * @returns {Object[]}
+	 */
 	RaceData.prototype.toPieChartSeries = function () {
 		var race, item, output = [], total, label;
 
@@ -45,6 +61,9 @@ define(function () {
 		return output;
 	};
 
+	/** Generates an HTML Table of the race data.
+	 * @returns {HTMLTableElement}
+	 */
 	RaceData.prototype.toHtmlTable = function () {
 		var self = this, table, tbody, innerHtml, total, propertyName;
 
