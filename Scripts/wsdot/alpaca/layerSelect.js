697726308fdcfe78005041c1caeaecfa450e06a6
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-03-18 17:28:03 -0700

    Started work on querying features from layer.

--- Scripts/wsdot/alpaca/layerSelect.js
+++ Scripts/wsdot/alpaca/layerSelect.js
@@ -7,11 +7,24 @@ define([
 	"esri/tasks/QueryTask"
 ], function (declare, Evented, Deferred, request, QueryTask) {
 
+	/**
+	 * @typedef {Object} LayerInfo
+	 * @property {number} maxRecordCount
+	 * @property {string} objectIdField
+	 * @property {Field[]} fields
+	 * @property {string} displayField
+	 */
+
+	/**
+	 * Gets information about a layer.
+	 * @param {(string|esri/layer/FeatureLayer)} urlOrFeatureLayer - Either the URL to a feature layer or a FeatureLayer object.
+	 * @returns {dojo/Deferred}
+	 */
 	function getLayerInfo(urlOrFeatureLayer) {
 		var deferred;
 		if (typeof urlOrFeatureLayer === "string") {
 			deferred = request({
-				url: url,
+				url: urlOrFeatureLayer,
 				content: {
 					f: "json"
 				}
@@ -22,6 +35,56 @@ define([
 		}
 
 		return deferred;
+	}
+
+	/** 
+	 * @param {number[]} objectIds
+	 * @param {number} maxRecordCount
+	 * @returns {Array.<Array.<number>>}
+	 */
+	function breakObjectIdsIntoGroups(objectIds, maxRecordCount) {
+		var output = [], currentGroup;
+
+		if (maxRecordCount == null) {
+			output.push(objectIds);
+		} else {
+			for (var i = 0, l = objectIds.length; i < l; i += 1) {
+				if (i % maxRecordCount === 0) {
+					currentGroup = [];
+					output.push(currentGroup);
+				}
+				currentGroup.push(objectIds[i]);
+			}
+		}
+
+		return output;
+	}
+
+	/**
+	 * @property {(LayerInfo|FeatureLayer)} layerInfo
+	 * @property {QueryTask} [queryTask=undefined] - This property is only necessary if layerInfo is not a FeatureLayer.
+	 * @returns {dojo/Deferred}
+	 */
+	function getAllFeaturesForLayer(layerInfo, queryTask) {
+		var query, deferred, idsDeferred;
+
+		/** Reject the output deferred object. */
+		function handleError(err) {
+			deferred.reject(err);
+		}
+
+		deferred = new Deferred();
+		query = new Query();
+		query.where = "1=1"; // Return all features.
+		idsDeferred = layerInfo.queryIds ? layerInfo.queryIds(query) : (queryTask && queryTask.executeForIds) ? queryTask.executeForIds(query) : null;
+		if (!idsDeferred) {
+			throw new TypeError("Invalid parameters");
+		}
+		idsDeferred.then(function (objectIds) {
+			deferred.progress("retrieved object ids");
+			var oidGroups = breakObjectIdsIntoGroups(objectIds, layerInfo.maxRecordCount);
+			// TODO: query each set of OID groups for features.
+		},handleError);
 	}
 
 	return declare([Evented], {
@@ -61,6 +124,10 @@ define([
 				// Create a query task for the first child layer.
 				this.queryTask = new QueryTask([layer.url.trimRight("/"), "0"].join("/"));
 			}
+
+			getLayerInfo(this.queryTask ? this.queryTask.url : this.layer).then(function (layerInfo) {
+				// TODO: Query all features.
+			});
 		}
 	});
 });
